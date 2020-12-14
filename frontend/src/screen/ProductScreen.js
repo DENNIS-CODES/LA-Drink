b46ac5532da6d64ae765f4419bd2fb490e6aff03
Dennis Mwangi <dennistrevor06@gmail.com>
2020-12-14 23:56:32 +0300

    product render

--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import data from '../data';
+import { link }
 
 function ProductScreen (props) {
     console.log(props.match.params.id);
     const product = data.products.find(x =>  x.id === props.match.params.id);
-    return <div>
-        <h1>{product.name}</h1>
+    return <div className="details">
+        <div>
+            <Link to="/">Back to Result</Link>
+            </div>
+            <div className="details">
+                <div className="details-image">
+                    <img src={product.image} alt="product"></img>
+                </div>
+                <div className="details-info">
+                    <ul>
+                        <li>
+                           <h4>{product.name}</h4> 
+                        </li>
+                        <li>
+                            {product.rating} stars ({product.numReviews} Reviews)
+                        </li>
+                        <li>
+                            <b>{product.price}</b>
+                        </li>
+                        <li>
+                            Description:
+                            <div>
+                                {product.description}
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        
     </div>
 }
 export default ProductScreen;
