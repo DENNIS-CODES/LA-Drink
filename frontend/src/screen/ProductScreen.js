1c7989c233240cbf175f3c6345f79f14928331f9
Dennis Mwangi <dennistrevor06@gmail.com>
2020-12-14 23:46:16 +0300

    data.js

--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import data from '../data';
 
 function ProductScreen (props) {
-    return <div>Shop</div>
+    console.log(props.match.params.id);
+    const product = data.products.find(x =>  x.id === props.match.params.id);
+    return <div>
+        <h1>{product.name}</h1>
+    </div>
 }
 export default ProductScreen;
