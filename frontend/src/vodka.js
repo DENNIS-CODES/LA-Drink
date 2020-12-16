f9a002666d60ae2571cc97f6f65aec4e332091c2
Dennis Mwangi <dennistrevor06@gmail.com>
2020-12-16 22:41:48 +0300

    vodka

---
+++ frontend/src/vodka.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import data from '../data';
+import { Link } from 'react-router-dom';
+
+function vodka (props) {
+    return <ul className="products">
+    {
+      data.products.map(product =>
+        <li>
+      <div className="product">
+      <Link to={'/product/' + product._id}>
+        <img  className="product-image" src={product.image} alt="wine"/>
+        </Link>
+      <div className="product-name">
+          <Link to={'/product/' + product._id}>{product.name}</Link></div>
+      <div className="product-brand">{product.brand}</div>
+      <div className="product-price">ksh{product.price}</div>
+        <div className="product-rating">{product.rating} Stars (10 Reviews)</div>
+        </div>
+        </li>
+        )
+      }
+      </ul>
+}
+export default vodka; 
