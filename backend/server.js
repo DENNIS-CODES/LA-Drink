d5100cac3b7ebf3895cb023abfff3faa580cc121
Dennis Mwangi <dennistrevor06@gmail.com>
2020-12-17 23:00:51 +0300

    server.js

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,5 @@ const app =express();
 app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
+
+app.listen(5000, () => { console.log("server started at http://localhost:5000") });
