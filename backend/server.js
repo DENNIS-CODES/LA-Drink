d09acfd0a9284fd85d69b8fb8d05dd5c4849b726
Dennis Mwangi <dennistrevor06@gmail.com>
2020-12-20 00:15:43 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,13 @@ import data from './data';
 
 const app =express();
 
-app.get("/api/products", (req, res) => {
-    res.send(data.products);
+app.get("/api/products/:id", (req, res) => {
+    const productId = req.params.id;
+    const product = data.products.find(x=>x._id === productId);
+    if(product)
+    res.send(product);
+    else
+    res.status(404).send({ msg: ""})
 });
 
 app.listen(5000, () => { console.log("server started at http://localhost:5000") });
