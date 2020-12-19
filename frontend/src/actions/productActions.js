a29ae55c9572f11a8ee8d1c8573b9df64bdd1436
Dennis Mwangi <dennistrevor06@gmail.com>
2020-12-19 23:12:14 +0300

    constants

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,3 +1,7 @@
-const listProduct = () => (dispatch) => {
-    
+import Axios from "axios";
+import { PRODUCT_LIST_REQUEST } from "../constants/productConstants"
+const listProduct = () => async (dispatch) => {
+    dispatch(PRODUCT_LIST_REQUEST);
+    const { data } = await Axios.get("api/products");
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
 }
