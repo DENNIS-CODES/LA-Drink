8a7c08a6fa45cbcdcb7b1e0b0076482f11c4dba5
Dennis Mwangi <dennistrevor06@gmail.com>
2020-12-19 23:19:07 +0300

    actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,7 +1,15 @@
 import Axios from "axios";
 import { PRODUCT_LIST_REQUEST } from "../constants/productConstants"
 const listProduct = () => async (dispatch) => {
-    dispatch(PRODUCT_LIST_REQUEST);
+    try{
+        dispatch(PRODUCT_LIST_REQUEST);
     const { data } = await Axios.get("api/products");
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+    }
+    catch(error){
+        dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
+    }
+   
 }
+
+export { listProduct }
--- frontend/src/screen/ShopScreen.js
+++ frontend/src/screen/ShopScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from "../actions/productActions";
 
 
 
