8976079358fcc81186d8eea8a3a5a35931a26135
Dennis Mwangi <dennistrevor06@gmail.com>
2020-12-20 00:26:29 +0300

    actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,7 @@
 
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants"
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL,
+PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from "../constants/productConstants"
+
 import axios from "axios";
 
 const listProducts = () => async (dispatch) => {
@@ -14,9 +16,9 @@ const listProducts = () => async (dispatch) => {
    
 }
 
-const detailsProduct = (productId) => (dispatch) =>  {
+const detailsProduct = (productId) => async (dispatch) =>  {
     try {
-        dispatch({type: PRODUCT_DETAILS_REQuesT,payload: productId});
+        dispatch({type: PRODUCT_DETAILS_REQUEST, payload: productId});
         const { data } = await axios.get("/api/products/" + productId);
         dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
     } catch (error) {
