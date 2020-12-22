10a4f2478d0d89261919376be797d3c3500073dd
Dennis Mwangi <dennistrevor06@gmail.com>
2020-12-23 00:12:49 +0300

    reducers

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,12 @@
 function cartReducer(state={cartItems:[]}, action){
     switch(action,type){
         case ADD_TO_CART:
-            
+            const item = action.payload;
+            const product = state.cartItems.find(x=> x.product === item.product);
+            if(product){
+            return {...state, cartItems:   state.cartItems.map(x=>x.product === product.product ? item : x)
+            };
+        }
+            return { cartITems: [...state.cartItems, item] };
     }
 }
