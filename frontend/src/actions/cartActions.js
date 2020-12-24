24a3cab6880598be2a97bbf3937f7cc86a08edc1
Dennis Mwangi <dennistrevor06@gmail.com>
2020-12-24 17:07:55 +0300

    cache

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -3,7 +3,7 @@ import { CART_ADD_ITEM } from '../constants/cartConstants';
 
 const addToCart = (productId, qty) => async (dispatch) => {
     try {
-        const { data } = await Axios.get("/api/products/" + productId);
+        const { data } = await Axios.get(`/api/products/${productId}`);
         dispatch({
             type : CART_ADD_ITEM, Payload: {
             product: data._id,
