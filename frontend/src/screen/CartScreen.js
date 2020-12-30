e6af0b6697bcce62b748ba7bb114b932db498ca2
Dennis Mwangi <dennistrevor06@gmail.com>
2020-12-30 20:21:34 +0300

    cart

--- frontend/src/screen/CartScreen.js
+++ frontend/src/screen/CartScreen.js
@@ -13,6 +13,9 @@ function CartScreen(props){
     const productId = props.match.params.id;
     const qty = props.location.search ? Number(props.location.search.split("=")[1]) : 1;
     const dispatch = useDispatch();
+    const removeFromCartHandler = (productId) => {
+
+    }
     useEffect(() => {
         if (productId) {
             dispatch(addToCart(productId, qty));
@@ -61,7 +64,7 @@ function CartScreen(props){
                   </div>
                 </div>
                 <div className="cart-price">
-                  ${item.price}
+                  ksh{item.price}
                 </div>
               </li>
             )
@@ -73,7 +76,7 @@ function CartScreen(props){
       <h3>
         Subtotal ( {cartItems.reduce((a, c) => a + c.qty, 0)} items)
         :
-         $ {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+         ksh {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
       </h3>
       <button onClick={checkoutHandler} className="button primary full-width" disabled={cartItems.length === 0}>
         Proceed to Checkout
