651db3f6262de19138dedcd3bbf70916ab5178c7
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-10 23:19:35 +0300

    redux-thunk

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,6 +10,6 @@ const reducer = combineReducers({
     cart: cartReducer
 })
 
-const composeEnhancer = window.__REDUX_DEVTOOL_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 export default store;
