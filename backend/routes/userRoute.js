d822111b2da173824e4f68fd504258e9208a5691
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-12 00:14:05 +0300

    Create Admin

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,11 +2,19 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get("/api/users/createadmin", async (req, res) =>{
-    const user = new user({
+router.get("/createadmin", async (req, res) =>{
+    try {
+         const user = new user({
         name: 'Deno',
         email: 'dennistrevor41@gmail.com',
         password: '1234',
         isAdmin: true
     });
-})
+    const newUser = await user.save();
+    res.send(user);
+    } catch (error) {
+        res.send({ msg: error.message });
+    }
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import data from './data';
 import dotenv from "dotenv";
 import config from './config';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
 
 dotenv.config();
 
@@ -12,6 +14,7 @@ Mongoose.connect(mongodbUrl, {
 
 const app =express();
 
+app.use("/api/users", userRoute);
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
     const product = data.products.find(x=>x._id === productId);
