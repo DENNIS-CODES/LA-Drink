7e9f495a14b1da848b373c35696f64ef63b6f1b3
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-12 23:30:29 +0300

    sign in

--- frontend/src/screen/SigninScreen.js
+++ frontend/src/screen/SigninScreen.js
@@ -4,9 +4,8 @@ import { Link } from 'react-router-dom';
 import { detailsProduct } from '../actions/productActions';
 
 function SigninScreen(props) {
-    const [qty, setQty] = useState(1);
-    const productDetails = useSelector(state => state.productDetails);
-    const { product, loading, error } = productDetails;
+    const [email, setEmail] = useState('');
+    const [ password, setPassword] = useState('');
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -15,6 +14,10 @@ function SigninScreen(props) {
             //
         };
     }, []);
+    const submitHandler = (e) =>{
+       e.preventDefault(); 
+    }
+    
     
     return <div classname="form">
         <form onSubmit={submitHandler} >
@@ -38,7 +41,7 @@ function SigninScreen(props) {
                     New to La-Drink?
                 </li>
                 <li>
-                    <Link to="/register" className>Create your La-Drink account</Link>
+                    <Link to="/register" className="button-fullwin">Create your La-Drink account</Link>
                 </li>
             </ul>
         </form>
