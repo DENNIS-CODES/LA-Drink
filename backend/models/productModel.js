9d3c3fb74eca34b90ea769dfc7e45116dbfb130c
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-13 23:43:07 +0300

    product Schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -4,9 +4,10 @@ const productSchema = new mongoose.Schema({
     name:{ type: String, required: true},
     image:{ type: String, required: true},
     brand:{ type: String, required: true},
-    category:{ type: String, required: true},
-    countInStock:{ type: String, required: true},
-    numReviews:{ type: String, required: true},
+    price:{ type: Number, default:0, required: true},
+    category:{ type: Number, default:0, required: true},
+    countInStock:{ type: Number, default: 0, required: true},
+    numReviews:{ type: Number, default: 0, required: true},
     
     
 });
