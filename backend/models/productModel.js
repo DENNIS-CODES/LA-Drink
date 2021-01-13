998a48ea22a9d33ed9a50fc0aa3d5810177d730c
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-13 23:35:50 +0300

    Product Schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,12 +1,12 @@
 import  mongoose  from 'mongoose';
 
-const userSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
     name:{ type: String, required: true},
     email: { type: String, required: true, unique: true},
     password: { type: String, required: true},
     isAdmin: { type: Boolean, required: true, default: false}
 });
 
-const userModel = mongoose.model("User", userSchema);
+const productModel = mongoose.model("product", productSchema);
 
-export default userModel;
+export default productModel;
