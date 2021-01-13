268af20a4f3eb20b0c145fdeed134889959e5968
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-13 23:45:07 +0300

    product route

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import User from '../models/userModel';
+
+const router = express.Router();
+
+router.post('/signin', async (req, res) =>{
+
+    const signinUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+    if(signinUser){
+        res.send({
+            _id: signinUser.id,
+            name: signinUser.name,
+            email: signinUser.email,
+            isAdmin: signinUser.isAdmin,
+            token: getToken(user)
+        })
+
+    } else {
+        res.status(401).send({msg:'invalid Email or Password.'});
+    }
+
+
+});
+
+router.get("/createadmin", async (req, res) =>{
+    try {
+         const user = new User({
+        name: 'Deno',
+        email: 'dennistrevor41@gmail.com',
+        password: '1234',
+        isAdmin: true
+    });
+    const newUser = await user.save();
+    res.send(newUser);
+    } catch (error) {
+        res.send({ msg: error.message });
+    }
+});
+
+export default router;
