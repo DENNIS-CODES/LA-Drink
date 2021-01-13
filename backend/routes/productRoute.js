3f50e965205233d2bd3bf4c411e8e490421081aa
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-13 23:49:54 +0300

    router function

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -3,6 +3,9 @@ import Product from '../models/productModel';
 
 const router = express.Router();
 
-
+router.get("/", async (req, res) =>{
+    const products = await Product.find({});
+    res.send(products);
+});
 
 export default router;
