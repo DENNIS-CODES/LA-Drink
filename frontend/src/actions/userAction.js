cf4cdaac2985e33035a41937bc2c99934ec3ebf0
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-14 22:25:08 +0300

    verifying user sign in

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,8 +1,10 @@
 const signin = (email, password) => (dispatch) => {
-    dispatch({type: USER_SIGNIN_REQUEST, payload: { email, password } });
+    dispatch({type: USER_SIGNIN_REQUEST, payload: { email, password }});
     try {
-        const { data } = await Axios.post("/api/users/signin", { email, password })
+        const { data } = await Axios.post("/api/users/signin", { email, password });
+        dispatch({type: USER_SIGN_SUCCESS, payload: data });
+        Cookie.set('userInfo', JSON.stringify(data));
     } catch (error) {
-        
+        dispatch({ type: USER_SIGN_FAIL, payload: error.message });
     }
 }
