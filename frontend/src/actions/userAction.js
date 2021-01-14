c137609d25676e07da59f80d6ef3f03056943824
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-14 23:38:45 +0300

    user Actions

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,8 +1,12 @@
+import Axios from "axios";
+import Cookie from 'js-cookie';
+import { USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGN_FAIL } from './constants/userConstants';
+
 const signin = (email, password) => (dispatch) => {
     dispatch({type: USER_SIGNIN_REQUEST, payload: { email, password }});
     try {
         const { data } = await Axios.post("/api/users/signin", { email, password });
-        dispatch({type: USER_SIGN_SUCCESS, payload: data });
+        dispatch({type: USER_SIGNIN_SUCCESS, payload: data });
         Cookie.set('userInfo', JSON.stringify(data));
     } catch (error) {
         dispatch({ type: USER_SIGN_FAIL, payload: error.message });
