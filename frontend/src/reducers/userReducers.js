05ce981d3181242d7ed210204fdc592d6462abf2
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-14 23:51:08 +0300

    get Registration  details

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -10,6 +10,19 @@ function userSigninReducer(state={}, action){
             return { loading: false, error: action.payload };
         default: return state;    
     }
-}export {
-    userSigninReducer
+}
+
+function useRegisterReducer(state={}, action){
+    switch(action.type) {
+        case USER_REGISTER_REQUEST:
+            return { loading: true };
+        case USER_REGISTER_SUCCESS:
+            return { loading: false, userInfo: action.payload };
+        case USER_REGISTER_FAIL:
+            return { loading: false, error: action.payload };
+        default: return state;    
+    }
+}
+export {
+    userSigninReducer, useRegisterReducer
 }
