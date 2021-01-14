89b16bc5ed963799975e8923455a2f97638a9a21
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-14 22:37:39 +0300

    checking Signin State

---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,15 @@
+import { USER_SIGNIN_REQUEST } from "../constants/userConstants";
+
+function userSigninReducer(state={}, action){
+    switch(action.type) {
+        case USER_SIGNIN_REQUEST:
+            return { loading: true };
+        case USER_SIGNIN_SUCCESS:
+            return { loading: false, userInfo: action.payload };
+        case USER_SIGNIN_FAIL:
+            return { loading: false, error: action.payload };
+        default: return state;    
+    }
+}export {
+    userSigninReducer
+}
