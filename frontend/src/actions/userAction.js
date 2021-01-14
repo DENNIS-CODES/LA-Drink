2c24221a085e8f8f8ee661908d73292f90f8102e
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-14 23:45:09 +0300

    getting user info

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -2,15 +2,26 @@ import Axios from "axios";
 import Cookie from 'js-cookie';
 import { USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGN_FAIL } from './constants/userConstants';
 
-const signin = (email, password) => (dispatch) => {
+const signin = (email, password) => async (dispatch) => {
     dispatch({type: USER_SIGNIN_REQUEST, payload: { email, password }});
     try {
         const { data } = await Axios.post("/api/users/signin", { email, password });
         dispatch({type: USER_SIGNIN_SUCCESS, payload: data });
         Cookie.set('userInfo', JSON.stringify(data));
     } catch (error) {
-        dispatch({ type: USER_SIGN_FAIL, payload: error.message });
+        dispatch({ type: USER_SIGNIN_FAIL, payload: error.message });
     }
 }
 
-export { signin };
+const register = (name, email, password) => async (dispatch) => {
+    dispatch({type: USER_REGISTER_REQUEST, payload: { name, email, password }});
+    try {
+        const { data } = await Axios.post("/api/users/register", { name, email, password });
+        dispatch({type: USER_REGISTER_SUCCESS, payload: data });
+        Cookie.set('userInfo', JSON.stringify(data));
+    } catch (error) {
+        dispatch({ type: USER_REGISTER_FAIL, payload: error.message });
+    }
+}
+
+export { signin, register };
