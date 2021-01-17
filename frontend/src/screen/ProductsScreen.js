9b19e3f0adb58021ed63f23d6de9eba5a9f863fd
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-17 23:58:17 +0300

    product screen

--- frontend/src/screen/ProductsScreen.js
+++ frontend/src/screen/ProductsScreen.js
@@ -155,4 +155,44 @@ function ProductsScreen(props) {
               </form>
             </div>
           )}
+
+<div className="product-list">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Category</th>
+              <th>Brand</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.brand}</td>
+                <td>
+                  <button className="button" onClick={() => openModal(product)}>
+                    Edit
+                  </button>{' '}
+                  <button
+                    className="button"
+                    onClick={() => deleteHandler(product)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
 export default ProductsScreen;
