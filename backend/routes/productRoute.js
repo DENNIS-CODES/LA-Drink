0fe0290c88d1d34f904bbbf59325f280e345d2b2
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-18 23:41:02 +0300

    product route render details

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -8,27 +8,6 @@ router.get("/", async (req, res) =>{
     res.send(products);
 });
 
-router.post("/", async(req, res) =>{
-    const product = new Product({
-        name: req.body.name,
-        price: req.body.price,
-        image: req.body.image,
-        brand: req.body.brand,
-        category: req.body.icategory,
-        countInStock: req.body.countInStock,
-        description: req.body.description,
-        rating: req.body.rating,
-        numReviews: req.body.numReviews, 
-    });
-    const newProduct = await product.save();
-    if (newProduct) {
-       return res
-       .status(201)
-       .send({ message:'New product Created', data: newProduct });
-    }
-    return res.status(500).send({ message: 'Error in Creating Product.' });
-});
-
 router.post("/:id", async(req, res) =>{
     const productId = req.params.id;
     const product = await product.findOne({_id: productId});
