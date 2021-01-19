c13d98fb1b8f75a05c11cc48f9a87cee3cfbf3ff
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-19 20:19:04 +0300

    configuring server API

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import path from 'path';
-import dotenv from "dotenv";
-import config from './config';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import config from './config';
 import userRoute from './routes/userRoute';
-import productRoute from './routes/userRoute';
-
-dotenv.config();
+import productRoute from './routes/productRoute';
+import orderRoute from './routes/orderRoute';
+import uploadRoute from './routes/uploadRoute';
 
 const mongodbUrl = config.MONGODB_URL;
 mongoose
@@ -18,19 +17,21 @@ mongoose
   })
   .catch((error) => console.log(error.reason));
 
-const app =express();
+const app = express();
 app.use(bodyParser.json());
-app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
-app.get("/api/products/:id", (req, res) => {
-//     const productId = req.params.id;
-//     const product = data.products.find(x=>x._id === productId);
-//     if(product)
-//     res.send(product);
-//     else
-//     res.status(404).send({ msg: "Product Not Found." })
-// 
+app.use('/api/uploads', uploadRoute);
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/orders', orderRoute);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(config.PAYPAL_CLIENT_ID);
 });
-app.listen(config.PORT, () => { 
-    console.log('server started at http://localhost:5000'); 
+app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+});
+
+app.listen(config.PORT, () => {
+  console.log('Server started at http://localhost:5000');
 });
