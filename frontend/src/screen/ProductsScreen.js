1141d347387cc995f93e1b7f3daa4f8866ccc166
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-19 12:42:18 +0300

    products screen

--- frontend/src/screen/ProductsScreen.js
+++ frontend/src/screen/ProductsScreen.js
@@ -1,181 +1,247 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { detailsProduct, saveProductReview } from '../actions/productActions';
-import Rating from '../components/Rating';
-import { PRODUCT_REVIEW_SAVE_RESET } from '../constants/productConstants';
+import axios from 'axios';
+import {
+  saveProduct,
+  listProducts,
+  deleteProdcut,
+} from '../actions/productActions';
 
-function ProductScreen(props) {
-  const [qty, setQty] = useState(1);
-  const [rating, setRating] = useState(0);
-  const [comment, setComment] = useState('');
-  const userSignin = useSelector((state) => state.userSignin);
-  const { userInfo } = userSignin;
-  const productDetails = useSelector((state) => state.productDetails);
-  const { product, loading, error } = productDetails;
-  const productReviewSave = useSelector((state) => state.productReviewSave);
-  const { success: productSaveSuccess } = productReviewSave;
+function ProductsScreen(props) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [image, setImage] = useState('');
+  const [brand, setBrand] = useState('');
+  const [category, setCategory] = useState('');
+  const [countInStock, setCountInStock] = useState('');
+  const [description, setDescription] = useState('');
+  const [uploading, setUploading] = useState(false);
+  const productList = useSelector((state) => state.productList);
+  const { loading, products, error } = productList;
+
+  const productSave = useSelector((state) => state.productSave);
+  const {
+    loading: loadingSave,
+    success: successSave,
+    error: errorSave,
+  } = productSave;
+
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    success: successDelete,
+    error: errorDelete,
+  } = productDelete;
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (productSaveSuccess) {
-      alert('Review submitted successfully.');
-      setRating(0);
-      setComment('');
-      dispatch({ type: PRODUCT_REVIEW_SAVE_RESET });
+    if (successSave) {
+      setModalVisible(false);
     }
-    dispatch(detailsProduct(props.match.params.id));
+    dispatch(listProducts());
     return () => {
       //
     };
-  }, [productSaveSuccess]);
+  }, [successSave, successDelete]);
+
+  const openModal = (product) => {
+    setModalVisible(true);
+    setId(product._id);
+    setName(product.name);
+    setPrice(product.price);
+    setDescription(product.description);
+    setImage(product.image);
+    setBrand(product.brand);
+    setCategory(product.category);
+    setCountInStock(product.countInStock);
+  };
   const submitHandler = (e) => {
     e.preventDefault();
-    // dispatch actions
     dispatch(
-      saveProductReview(props.match.params.id, {
-        name: userInfo.name,
-        rating: rating,
-        comment: comment,
+      saveProduct({
+        _id: id,
+        name,
+        price,
+        image,
+        brand,
+        category,
+        countInStock,
+        description,
       })
     );
   };
-  const handleAddToCart = () => {
-    props.history.push('/cart/' + props.match.params.id + '?qty=' + qty);
+  const deleteHandler = (product) => {
+    dispatch(deleteProdcut(product._id));
   };
-
+  const uploadFileHandler = (e) => {
+    const file = e.target.files[0];
+    const bodyFormData = new FormData();
+    bodyFormData.append('image', file);
+    setUploading(true);
+    axios
+      .post('/api/uploads', bodyFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((response) => {
+        setImage(response.data);
+        setUploading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setUploading(false);
+      });
+  };
   return (
-    <div>
-      <div className="back-to-result">
-        <Link to="/">Back to result</Link>
+    <div className="content content-margined">
+      <div className="product-header">
+        <h3>Products</h3>
+        <button className="button primary" onClick={() => openModal({})}>
+          Create Product
+        </button>
       </div>
-      {loading ? (
-        <div>Loading...</div>
-      ) : error ? (
-        <div>{error} </div>
-      ) : (
-        <>
-          <div className="details">
-            <div className="details-image">
-              <img src={product.image} alt="product"></img>
-            </div>
-            <div className="details-info">
-              <ul>
-                <li>
-                  <h4>{product.name}</h4>
-                </li>
-                <li>
-                  <a href="#reviews">
-                    <Rating
-                      value={product.rating}
-                      text={product.numReviews + ' reviews'}
-                    />
-                  </a>
-                </li>
-                <li>
-                  Price: <b>${product.price}</b>
-                </li>
-                <li>
-                  Description:
-                  <div>{product.description}</div>
-                </li>
-              </ul>
-            </div>
-            <div className="details-action">
-              <ul>
-                <li>Price: {product.price}</li>
-                <li>
-                  Status:{' '}
-                  {product.countInStock > 0 ? 'In Stock' : 'Unavailable.'}
-                </li>
-                <li>
-                  Qty:{' '}
-                  <select
-                    value={qty}
-                    onChange={(e) => {
-                      setQty(e.target.value);
-                    }}
-                  >
-                    {[...Array(product.countInStock).keys()].map((x) => (
-                      <option key={x + 1} value={x + 1}>
-                        {x + 1}
-                      </option>
-                    ))}
-                  </select>
-                </li>
-                <li>
-                  {product.countInStock > 0 && (
-                    <button
-                      onClick={handleAddToCart}
-                      className="button primary"
-                    >
-                      Add to Cart
-                    </button>
-                  )}
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="content-margined">
-            <h2>Reviews</h2>
-            {!product.reviews.length && <div>There is no review</div>}
-            <ul className="review" id="reviews">
-              {product.reviews.map((review) => (
-                <li key={review._id}>
-                  <div>{review.name}</div>
-                  <div>
-                    <Rating value={review.rating}></Rating>
-                  </div>
-                  <div>{review.createdAt.substring(0, 10)}</div>
-                  <div>{review.comment}</div>
-                </li>
-              ))}
+      {modalVisible && (
+        <div className="form">
+          <form onSubmit={submitHandler}>
+            <ul className="form-container">
               <li>
-                <h3>Write a customer review</h3>
-                {userInfo ? (
-                  <form onSubmit={submitHandler}>
-                    <ul className="form-container">
-                      <li>
-                        <label htmlFor="rating">Rating</label>
-                        <select
-                          name="rating"
-                          id="rating"
-                          value={rating}
-                          onChange={(e) => setRating(e.target.value)}
-                        >
-                          <option value="1">1- Poor</option>
-                          <option value="2">2- Fair</option>
-                          <option value="3">3- Good</option>
-                          <option value="4">4- Very Good</option>
-                          <option value="5">5- Excelent</option>
-                        </select>
-                      </li>
-                      <li>
-                        <label htmlFor="comment">Comment</label>
-                        <textarea
-                          name="comment"
-                          value={comment}
-                          onChange={(e) => setComment(e.target.value)}
-                        ></textarea>
-                      </li>
-                      <li>
-                        <button type="submit" className="button primary">
-                          Submit
-                        </button>
-                      </li>
-                    </ul>
-                  </form>
-                ) : (
-                  <div>
-                    Please <Link to="/signin">Sign-in</Link> to write a review.
-                  </div>
-                )}
+                <h2>Create Product</h2>
+              </li>
+              <li>
+                {loadingSave && <div>Loading...</div>}
+                {errorSave && <div>{errorSave}</div>}
+              </li>
+
+              <li>
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={name}
+                  id="name"
+                  onChange={(e) => setName(e.target.value)}
+                ></input>
+              </li>
+              <li>
+                <label htmlFor="price">Price</label>
+                <input
+                  type="text"
+                  name="price"
+                  value={price}
+                  id="price"
+                  onChange={(e) => setPrice(e.target.value)}
+                ></input>
+              </li>
+              <li>
+                <label htmlFor="image">Image</label>
+                <input
+                  type="text"
+                  name="image"
+                  value={image}
+                  id="image"
+                  onChange={(e) => setImage(e.target.value)}
+                ></input>
+                <input type="file" onChange={uploadFileHandler}></input>
+                {uploading && <div>Uploading...</div>}
+              </li>
+              <li>
+                <label htmlFor="brand">Brand</label>
+                <input
+                  type="text"
+                  name="brand"
+                  value={brand}
+                  id="brand"
+                  onChange={(e) => setBrand(e.target.value)}
+                ></input>
+              </li>
+              <li>
+                <label htmlFor="countInStock">CountInStock</label>
+                <input
+                  type="text"
+                  name="countInStock"
+                  value={countInStock}
+                  id="countInStock"
+                  onChange={(e) => setCountInStock(e.target.value)}
+                ></input>
+              </li>
+              <li>
+                <label htmlFor="name">Category</label>
+                <input
+                  type="text"
+                  name="category"
+                  value={category}
+                  id="category"
+                  onChange={(e) => setCategory(e.target.value)}
+                ></input>
+              </li>
+              <li>
+                <label htmlFor="description">Description</label>
+                <textarea
+                  name="description"
+                  value={description}
+                  id="description"
+                  onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+              </li>
+              <li>
+                <button type="submit" className="button primary">
+                  {id ? 'Update' : 'Create'}
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  onClick={() => setModalVisible(false)}
+                  className="button secondary"
+                >
+                  Back
+                </button>
               </li>
             </ul>
-          </div>
-        </>
+          </form>
+        </div>
       )}
+
+      <div className="product-list">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Category</th>
+              <th>Brand</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.brand}</td>
+                <td>
+                  <button className="button" onClick={() => openModal(product)}>
+                    Edit
+                  </button>{' '}
+                  <button
+                    className="button"
+                    onClick={() => deleteHandler(product)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
-export default ProductScreen;
+export default ProductsScreen;
