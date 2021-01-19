5c33ae9e4e341f980bcef8920bb091ffbe939a07
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-19 12:26:10 +0300

    Admin Creating new product

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -40,4 +40,27 @@ router.delete("/id", async(req, res) =>
   }
 });
 
+router.post('/', isAuth, isAdmin, async (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    price: req.body.price,
+    image: req.body.image,
+    brand: req.body.brand,
+    category: req.body.category,
+    countInStock: req.body.countInStock,
+    description: req.body.description,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+  });
+  const newProduct = await product.save();
+  if (newProduct) {
+    return res
+      .status(201)
+      .send({ message: 'New Product Created', data: newProduct });
+  }
+  return res.status(500).send({ message: ' Error in Creating Product.' });
+});
+
+export default router;
+
 export default router;
