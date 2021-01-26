350a644b15514187cb73ca0fff582486a6f96bf7
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-26 22:57:57 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -17,21 +17,21 @@ mongoose
   })
   .catch((error) => console.log(error.reason));
 
-const app = express();
-app.use(bodyParser.json());
-app.use('/api/uploads', uploadRoute);
-app.use('/api/users', userRoute);
-app.use('/api/products', productRoute);
-app.use('/api/orders', orderRoute);
-app.get('/api/config/paypal', (req, res) => {
-  res.send(config.PAYPAL_CLIENT_ID);
-});
-app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
-app.use(express.static(path.join(__dirname, '/../frontend/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
-});
+  const app = express();
+  app.use(bodyParser.json());
+  app.use('/api/uploads', uploadRoute);
+  app.use('/api/users', userRoute);
+  app.use('/api/products', productRoute);
+  app.use('/api/orders', orderRoute);
+  app.get('/api/config/paypal', (req, res) => {
+    res.send(config.PAYPAL_CLIENT_ID);
+  });
+  app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+  app.use(express.static(path.join(__dirname, '/../frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+  });
 
-app.listen(config.PORT, () => {
-  console.log('Server started at http://localhost:5000');
-});
+  app.listen(config.PORT, () => {
+    console.log('Server started at http://localhost:5000');
+  });
--- frontend/src/screen/ShippingScreen.js
+++ frontend/src/screen/ShippingScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { saveShipping } from '../actions/cartAction';
+import { saveShipping } from '../actions/cartActions';
 
 function ShippingScreen(props) {
 
