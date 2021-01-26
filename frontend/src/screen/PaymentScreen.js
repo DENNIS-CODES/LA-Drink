2be614f50cfd8561a63aedf686137c1ccbd139b2
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-26 23:16:09 +0300

    bug fixes

--- frontend/src/screen/PaymentScreen.js
+++ frontend/src/screen/PaymentScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { savePayment } from '../actions/cartAction';
+import { savePayment } from '../actions/cartActions';
 import CheckoutSteps from '../components/CheckoutStep'
 
 function PaymentScreen(props) {
--- frontend/src/screen/ShippingScreen.js
+++ frontend/src/screen/ShippingScreen.js
@@ -2,24 +2,23 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { saveShipping } from '../actions/cartActions';
+import CheckoutSteps from '../components/CheckoutStep';
 
 function ShippingScreen(props) {
 
-    const [address, setAddress] = useState('');
-    const [city, setCity] = useState('');
-    const [postalCode, setPostalCode] = useState('');
-    const [country, setCountry] = useState('');
-  
-    const dispatch = useDispatch();
-  
-    const submitHandler = (e) => {
-      e.preventDefault();
-      dispatch(saveShipping( address, city, postalCode, country ));
-      props.history.push('payment');
-    }
-    
-    
-    return <div>
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [country, setCountry] = useState('');
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(saveShipping({ address, city, postalCode, country }));
+    props.history.push('payment');
+  }
+  return <div>
     <CheckoutSteps step1 step2 ></CheckoutSteps>
     <div className="form">
       <form onSubmit={submitHandler} >
@@ -56,13 +55,16 @@ function ShippingScreen(props) {
             <input type="text" name="country" id="country" onChange={(e) => setCountry(e.target.value)}>
             </input>
           </li>
-                <li>
-                    <button type="submit" className="button-primary">Continue</button>
-                </li>
-            </ul>
-        </form>
+
+
+          <li>
+            <button type="submit" className="button primary">Continue</button>
+          </li>
+
+        </ul>
+      </form>
     </div>
-    </div>
+  </div>
 
 }
 export default ShippingScreen;
