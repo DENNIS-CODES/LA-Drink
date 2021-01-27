4bb1d016dd8fc8171b0d30385459cb16504edbe4
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-27 13:41:45 +0300

    product rating

--- frontend/src/screen/ShopScreen.js
+++ frontend/src/screen/ShopScreen.js
@@ -1,7 +1,5 @@
-  
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 import { listProducts } from '../actions/productActions';
 import Rating from '../components/Rating';
