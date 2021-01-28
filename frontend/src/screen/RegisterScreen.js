65b09cffdaa6eba1063f6badb18a6099cc3c742b
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-28 16:08:26 +0300

    multer

--- frontend/src/screen/RegisterScreen.js
+++ frontend/src/screen/RegisterScreen.js
@@ -67,7 +67,7 @@ function RegisterScreen(props) {
                 </li>
                 <li>
           Already have an account?
-          <Link to={redirect === "/" ? "signin" : "signin?redirect=" + redirect} className="button secondary text-center" >Create your La-Drink account</Link>
+          <Link to={redirect === "/" ? "signin" : "signin?redirect=" + redirect} className="button secondary text-center" >Sign in</Link>
 
         </li>
 
