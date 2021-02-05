d88a5fe4aa4d77782c20b3399fec2fd0c5ed8008
Dennis Mwangi <dennistrevor06@gmail.com>
2021-02-05 21:17:36 +0300

    order Products API

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -7,5 +7,15 @@ orderRouter.post(
      expressAsyncHandler(async (re, res) => {
          if (req.body.orderItems.length === 0) {
              res.status(400).send({ message: 'cart is empty'});
+         } else {
+             const Oder = new Oder({
+                 orderItems: req.body.oderItems,
+                 shippingAddress: req.body.shippingAddress,
+                 paymentMethod: req.body.paymentMethod,
+                 itemsPrice: req.body.itemsPrice,
+                 shippingPrice: req.body.shippingPrice,
+                 texPrice: req.body.texPrice,
+                 totalPrice: req.body.totalPrice, 
+             })
          }
      }))
