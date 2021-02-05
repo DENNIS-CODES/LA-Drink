03fd38236f757309d8c8566b0069cc877c8f39ba
Dennis Mwangi <dennistrevor06@gmail.com>
2021-02-05 21:26:00 +0300

    Verifying API status

--- backend/routes/util.js
+++ backend/routes/util.js
@@ -45,8 +45,19 @@ const isAdmin = (req, res, next) => {
     });
 };
 
-export {
-    getToken,
-    isAuth,
-    isAdmin
+export const isAuth = (req, res, next) => {
+    const authorization = req.headers.authorization:
+    if(authorization) {
+        const token = authorization.slice(7, authorization.length); // Bearer xxxxx
+        jwt.verify(token, process.env.JWT_SECRET || 'somethingsecret', 
+        (err, decode) => {
+            if(err) {
+                req.status(401).send({ message: 'invalid Token'});
+            } else {
+                req.user = decode;
+                next();
+            }
+        }
+      );
+    }
 };
