0699ba61322524a07d69a6e2e7904403ce1f2a20
Dennis Mwangi <dennistrevor06@gmail.com>
2021-02-18 15:17:29 +0300

    implementing cart action

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,41 +1,52 @@
-import Axios from "axios";
-import Cookie from "js-cookie";
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING, CART_SAVE_PAYMENT } from "../constants/cartConstants";
+import Axios from 'axios';
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
+  CART_ADD_ITEM_FAIL,
+} from '../constants/cartConstants';
 
-const addToCart = (productId, qty) => async (dispatch, getState) => {
-    try {
-      const { data } = await Axios.get("/api/products/" + productId);
-      dispatch({
-        type: CART_ADD_ITEM, payload: {
-          product: data._id,
-          name: data.name,
-          image: data.image,
-          price: data.price,
-          countInStock: data.countInStock,
-          qty
-        }
-      });
-      const { cart: { cartItems } } = getState();
-      Cookie.set("cartItems", JSON.stringify(cartItems));
-  
-    } catch (error) {
-  
-    }
+export const addToCart = (productId, qty) => async (dispatch, getState) => {
+  const { data } = await Axios.get(`/api/products/${productId}`);
+  const {
+    cart: { cartItems },
+  } = getState();
+  if (cartItems.length > 0 && data.seller._id !== cartItems[0].seller._id) {
+    dispatch({
+      type: CART_ADD_ITEM_FAIL,
+      payload: `Can't Add To Cart. Buy only from ${cartItems[0].seller.seller.name} in this order`,
+    });
+  } else {
+    dispatch({
+      type: CART_ADD_ITEM,
+      payload: {
+        name: data.name,
+        image: data.image,
+        price: data.price,
+        countInStock: data.countInStock,
+        product: data._id,
+        seller: data.seller,
+        qty,
+      },
+    });
+    localStorage.setItem(
+      'cartItems',
+      JSON.stringify(getState().cart.cartItems)
+    );
   }
-  const removeFromCart = (productId) => (dispatch, getState) => {
-    dispatch({ type: CART_REMOVE_ITEM, payload: productId });
-  
-    const { cart: { cartItems } } = getState();
-    Cookie.set("cartItems", JSON.stringify(cartItems));
-  }
-  const saveShipping = (data) => (dispatch) => {
-    dispatch({ type: CART_SAVE_SHIPPING, payload: data });
-  }
-  const savePayment = (data) => (dispatch) => {
-    dispatch({ type: CART_SAVE_PAYMENT, payload: data });
-  }
-export const removeFromCart = (productId) => (dispatch, getState) => { 
-  dispatchEvent({
-    type: CART_REMOVE_ITEM, payload: productId
-  })
- }; 
+};
+
+export const removeFromCart = (productId) => (dispatch, getState) => {
+  dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
+  localStorage.setItem('shippingAddress', JSON.stringify(data));
+};
+export const savePaymentMethod = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_PAYMENT_METHOD, payload: data });
+};
+
