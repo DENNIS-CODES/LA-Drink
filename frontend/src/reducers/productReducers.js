df9432059b169bddd1373b9ecabbb13837d1dfb4
Dennis Mwangi <dennistrevor06@gmail.com>
2021-03-04 17:24:26 +0300

    Using redux to manage products

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,59 +1,133 @@
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL,
-    PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_SAVE_REQUEST, PRODUCT_SAVE_SUCCESS, PRODUCT_SAVE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL } from "../constants/productConstants";
-function productListReducer( state = { products: [] }, action) {
-
+const {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_CREATE_REQUEST,
+    PRODUCT_CREATE_SUCCESS,
+    PRODUCT_CREATE_FAIL,
+    PRODUCT_CREATE_RESET,
+    PRODUCT_UPDATE_REQUEST,
+    PRODUCT_UPDATE_SUCCESS,
+    PRODUCT_UPDATE_FAIL,
+    PRODUCT_UPDATE_RESET,
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAIL,
+    PRODUCT_DELETE_RESET,
+    PRODUCT_CATEGORY_LIST_REQUEST,
+    PRODUCT_CATEGORY_LIST_SUCCESS,
+    PRODUCT_CATEGORY_LIST_FAIL,
+    PRODUCT_REVIEW_CREATE_REQUEST,
+    PRODUCT_REVIEW_CREATE_SUCCESS,
+    PRODUCT_REVIEW_CREATE_FAIL,
+    PRODUCT_REVIEW_CREATE_RESET,
+  } = require('../constants/productConstants');
+  
+  export const productListReducer = (
+    state = { loading: true, products: [] },
+    action
+  ) => {
     switch (action.type) {
-        case PRODUCT_LIST_REQUEST:
-        return { loading: true, products: [] };
-       case PRODUCT_LIST_SUCCESS:
-           return { loading: false, products: action.payload };
-           case PRODUCT_LIST_FAIL:
-               return { loading: false, error: action.payload }
-               default:
-                   return state; 
-    }
-}
-
-function productDetailsReducer(state = { product: {} }, action) {
-
-    switch (action.type) {
-        case PRODUCT_DETAILS_REQUEST:
+      case PRODUCT_LIST_REQUEST:
         return { loading: true };
-       case PRODUCT_DETAILS_SUCCESS:
-           return { loading: false, product: action.payload };
-           case PRODUCT_DETAILS_FAIL:
-               return { loading: false, error: action.payload }
-               default:
-                   return state; 
+      case PRODUCT_LIST_SUCCESS:
+        return {
+          loading: false,
+          products: action.payload.products,
+          pages: action.payload.pages,
+          page: action.payload.page,
+        };
+      case PRODUCT_LIST_FAIL:
+        return { loading: false, error: action.payload };
+      default:
+        return state;
     }
-}
-
-function productSaveReducer(state = { product: {} }, action) {
-
+  };
+  
+  export const productCategoryListReducer = (
+    state = { loading: true, products: [] },
+    action
+  ) => {
     switch (action.type) {
-        case PRODUCT_SAVE_REQUEST:
+      case PRODUCT_CATEGORY_LIST_REQUEST:
         return { loading: true };
-       case PRODUCT_SAVE_SUCCESS:
-           return { loading: false, success: true, product: action.payload };
-           case PRODUCT_SAVE_FAIL:
-               return { loading: false, error: action.payload }
-               default:
-                   return state; 
+      case PRODUCT_CATEGORY_LIST_SUCCESS:
+        return { loading: false, categories: action.payload };
+      case PRODUCT_CATEGORY_LIST_FAIL:
+        return { loading: false, error: action.payload };
+      default:
+        return state;
     }
-}
-
-function productDeleteReducer(state = { product: {} }, action) {
-
+  };
+  
+  export const productDetailsReducer = (state = { loading: true }, action) => {
     switch (action.type) {
-        case PRODUCT_DELETE_REQUEST:
+      case PRODUCT_DETAILS_REQUEST:
         return { loading: true };
-       case PRODUCT_DELETE_SUCCESS:
-           return { loading: false, success: true, product: action.payload, success: true };
-           case PRODUCT_DELETE_FAIL:
-               return { loading: false, error: action.payload }
-               default:
-                   return state; 
+      case PRODUCT_DETAILS_SUCCESS:
+        return { loading: false, product: action.payload };
+      case PRODUCT_DETAILS_FAIL:
+        return { loading: false, error: action.payload };
+      default:
+        return state;
     }
-}
-
-export { productListReducer, productDetailsReducer, productSaveReducer, productDeleteReducer }
+  };
+  export const productCreateReducer = (state = {}, action) => {
+    switch (action.type) {
+      case PRODUCT_CREATE_REQUEST:
+        return { loading: true };
+      case PRODUCT_CREATE_SUCCESS:
+        return { loading: false, success: true, product: action.payload };
+      case PRODUCT_CREATE_FAIL:
+        return { loading: false, error: action.payload };
+      case PRODUCT_CREATE_RESET:
+        return {};
+      default:
+        return state;
+    }
+  };
+  export const productUpdateReducer = (state = {}, action) => {
+    switch (action.type) {
+      case PRODUCT_UPDATE_REQUEST:
+        return { loading: true };
+      case PRODUCT_UPDATE_SUCCESS:
+        return { loading: false, success: true };
+      case PRODUCT_UPDATE_FAIL:
+        return { loading: false, error: action.payload };
+      case PRODUCT_UPDATE_RESET:
+        return {};
+      default:
+        return state;
+    }
+  };
+  export const productDeleteReducer = (state = {}, action) => {
+    switch (action.type) {
+      case PRODUCT_DELETE_REQUEST:
+        return { loading: true };
+      case PRODUCT_DELETE_SUCCESS:
+        return { loading: false, success: true };
+      case PRODUCT_DELETE_FAIL:
+        return { loading: false, error: action.payload };
+      case PRODUCT_DELETE_RESET:
+        return {};
+      default:
+        return state;
+    }
+  };
+  export const productReviewCreateReducer = (state = {}, action) => {
+    switch (action.type) {
+      case PRODUCT_REVIEW_CREATE_REQUEST:
+        return { loading: true };
+      case PRODUCT_REVIEW_CREATE_SUCCESS:
+        return { loading: false, success: true, review: action.payload };
+      case PRODUCT_REVIEW_CREATE_FAIL:
+        return { loading: false, error: action.payload };
+      case PRODUCT_REVIEW_CREATE_RESET:
+        return {};
+      default:
+        return state;
+    }
+  };
--- frontend/src/screen/CartScreen.js
+++ frontend/src/screen/CartScreen.js
@@ -1,23 +1,26 @@
 import React, { useEffect } from 'react';
-import { addToCart, removeFromCart } from '../actions/cartActions';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { addToCart, removeFromCart } from '../actions/cartActions';
+import MessageBox from '../components/MessageBox';
 
 export default function CartScreen(props) {
   const productId = props.match.params.id;
   const qty = props.location.search
-  ? Number(props.loaction.search.split('=')[1])
-  :1;
-  const cart = useSelector(state => state.cart);
-  const { cartItems } = cart;
+    ? Number(props.location.search.split('=')[1])
+    : 1;
+  const cart = useSelector((state) => state.cart);
+  const { cartItems, error } = cart;
   const dispatch = useDispatch();
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty));
     }
-  },[dispatch, productId, qty]);
+  }, [dispatch, productId, qty]);
+
   const removeFromCartHandler = (id) => {
-    //delete action
+    // delete action
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
@@ -27,60 +30,73 @@ export default function CartScreen(props) {
     <div className="row top">
       <div className="col-2">
         <h1>Shopping Cart</h1>
-        {cartItems.length === 0?<MessageBox>
-          Cart is empty. <Link to="/">Go Shopping</Link>
-        </MessageBox>
-        :
-        (
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
+        {cartItems.length === 0 ? (
+          <MessageBox>
+            Cart is empty. <Link to="/">Go Shopping</Link>
+          </MessageBox>
+        ) : (
           <ul>
-            {
-              cartItems.maps((item) => {
-                <li key={item.product}>
-                  <div className="row">
-                    <div>
-                      <img 
-                         src={item.image}
-                         alt={item.name}
-                         className="email">
-                         </img>
-                    </div>
-                    <div className="min-30">
-                      <Link to={`/${item.product}`}>{item.name}</Link>
-                    </div>
-                    <div>
-                      <select value={item.qty} onChange={e => dispatch(
-                        addToCart(item.product), Number(e.target.value)
-                      )
-                    }>
-                      {[...Array(product.countInStock).keys()].map(
+            {cartItems.map((item) => (
+              <li key={item.product}>
+                <div className="row">
+                  <div>
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      className="small"
+                    ></img>
+                  </div>
+                  <div className="min-30">
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </div>
+                  <div>
+                    <select
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
                         <option key={x + 1} value={x + 1}>
                           {x + 1}
                         </option>
-                      )}
+                      ))}
                     </select>
-                    </div>
-                    <div>${item.price}</div>
-                    <div>
-                      <button type="button" onClick={() => removeFromCartHandler(item.product)}>
-                        Delete
-                      </button>
-                    </div>
                   </div>
-                </li>
-              })
-            }
+                  <div>${item.price}</div>
+                  <div>
+                    <button
+                      type="button"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
           </ul>
-        )
-      }
+        )}
       </div>
-      <div className="col-l">
+      <div className="col-1">
         <div className="card card-body">
           <ul>
             <li>
-              subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : ${cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+              <h2>
+                Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : $
+                {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+              </h2>
             </li>
             <li>
-              <button type="button" onClick={checkoutHandler} className="primary block" disabled={cartItems.length === 0}>
+              <button
+                type="button"
+                onClick={checkoutHandler}
+                className="primary block"
+                disabled={cartItems.length === 0}
+              >
                 Proceed to Checkout
               </button>
             </li>
@@ -88,6 +104,5 @@ export default function CartScreen(props) {
         </div>
       </div>
     </div>
-  )
-};
-export default CartScreen;
+  );
+}
