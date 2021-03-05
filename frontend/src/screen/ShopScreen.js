92b48d8d338e44ba963996b48c6cf9fd87c921ff
Dennis Mwangi <dennistrevor06@gmail.com>
2021-03-05 15:54:02 +0300

    Creating Shop screen

--- frontend/src/screen/ShopScreen.js
+++ frontend/src/screen/ShopScreen.js
@@ -1,89 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+import Product from '../components/Product';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../actions/productActions';
-import Rating from '../components/Rating';
+import { listTopSellers } from '../actions/userActions';
+import { Link } from 'react-router-dom';
 
-function ShopScreen(props) {
-  const [searchKeyword, setSearchKeyword] = useState('');
-  const [sortOrder, setSortOrder] = useState('');
-  const category = props.match.params.id ? props.match.params.id : '';
-  const productList = useSelector((state) => state.productList);
-  const { products, loading, error } = productList;
+export default function ShopScreen() {
   const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  const userTopSellersList = useSelector((state) => state.userTopSellersList);
+  const {
+    loading: loadingSellers,
+    error: errorSellers,
+    users: sellers,
+  } = userTopSellersList;
+
   useEffect(() => {
-    dispatch(listProducts(category));
-
-    return () => {
-      //
-    };
-  }, [category]);
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    dispatch(listProducts(category, searchKeyword, sortOrder));
-  };
-  const sortHandler = (e) => {
-    setSortOrder(e.target.value);
-    dispatch(listProducts(category, searchKeyword, sortOrder));
-  };
-
+    dispatch(listProducts({}));
+    dispatch(listTopSellers());
+  }, [dispatch]);
   return (
-    <>
-      {category && <h2>{category}</h2>}
-
-      <ul className="filter">
-        <li>
-          <form onSubmit={submitHandler}>
-            <input
-              name="searchKeyword"
-              onChange={(e) => setSearchKeyword(e.target.value)}
-            />
-            <button type="submit">Search</button>
-          </form>
-        </li>
-        <li>
-          Sort By{' '}
-          <select name="sortOrder" onChange={sortHandler}>
-            <option value="">Newest</option>
-            <option value="lowest">Lowest</option>
-            <option value="highest">Highest</option>
-          </select>
-        </li>
-      </ul>
-      {loading ? (
-        <div>Loading...</div>
-      ) : error ? (
-        <div>{error}</div>
+    <div>
+      <h2>Top Sellers</h2>
+      {loadingSellers ? (
+        <LoadingBox></LoadingBox>
+      ) : errorSellers ? (
+        <MessageBox variant="danger">{errorSellers}</MessageBox>
       ) : (
-        <ul className="products">
-          {products.map((product) => (
-            <li key={product._id}>
-              <div className="product">
-                <Link to={'/product/' + product._id}>
-                  <img
-                    className="product-image"
-                    src={product.image}
-                    alt="product"
-                  />
+        <>
+          {sellers.length === 0 && <MessageBox>No Seller Found</MessageBox>}
+          <Carousel showArrows autoPlay showThumbs={false}>
+            {sellers.map((seller) => (
+              <div key={seller._id}>
+                <Link to={`/seller/${seller._id}`}>
+                  <img src={seller.seller.logo} alt={seller.seller.name} />
+                  <p className="legend">{seller.seller.name}</p>
                 </Link>
-                <div className="product-name">
-                  <Link to={'/product/' + product._id}>{product.name}</Link>
-                </div>
-                <div className="product-brand">{product.brand}</div>
-                <div className="product-price">${product.price}</div>
-                <div className="product-rating">
-                  <Rating
-                    value={product.rating}
-                    text={product.numReviews + ' reviews'}
-                  />
-                </div>
               </div>
-            </li>
-          ))}
-        </ul>
+            ))}
+          </Carousel>
+        </>
       )}
-    </>
+      <h2>Featured Products</h2>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <>
+          {products.length === 0 && <MessageBox>No Product Found</MessageBox>}
+          <div className="row center">
+            {products.map((product) => (
+              <Product key={product._id} product={product}></Product>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
   );
 }
-export default ShopScreen;
